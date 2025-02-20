a1c2893347b5fe32692da90c11331423ca437d8c
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-02-20 21:19:51 +0500

    Task 4

---
+++ script.js
@@ -0,0 +1,11 @@
+function deletTable() {
+    let confirmResponse = confirm("Are you sure you want to delete this list?");
+    let table1 = document.getElementById("table1");
+    
+    if (confirmResponse) {
+        table1.style.display = "none"; // Hide the div if confirmed
+        alert("List deleted!");
+    } else {
+        alert("Action canceled!");
+    }
+}
